512c237a2412cf16cb4f1e66fc22742487a6d4dd
adtokarz <adrian.tokarz@visionect.io>
2020-10-19 19:32:32 +0200

    update

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -18,16 +18,34 @@ function TodoForm(props) {
   };
 
   return (
-    <form className="todo-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add a todo"
-        value={input}
-        name="text"
-        className="todo-input"
-        onChange={handleChange}
-      />
-      <button className="todo-button">Add todo</button>
+    <form onSubmit={handleSubmit} className="todo-form">
+      {props.edit ? (
+        <>
+          <input
+            placeholder="Update your item"
+            value={input}
+            onChange={handleChange}
+            name="text"
+            className="todo-input edit"
+          />
+          <button onClick={handleSubmit} className="todo-button edit">
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder="Add a todo"
+            value={input}
+            onChange={handleChange}
+            name="text"
+            className="todo-input"
+          />
+          <button onClick={handleSubmit} className="todo-button">
+            Add todo
+          </button>
+        </>
+      )}
     </form>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import Todo from './Todo';
+// import './Todo.scss';
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
@@ -15,11 +16,21 @@ function TodoList() {
     console.log(...todos);
   };
 
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
   const removeTodo = (id) => {
     const removedArr = [...todos].filter((todo) => todo.id !== id);
 
     setTodos(removedArr);
   };
+
   const completeTodo = (id) => {
     let updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
@@ -31,10 +42,15 @@ function TodoList() {
   };
 
   return (
-    <div>
+    <div className="app-container">
       <h1>What's the plan for today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 }
